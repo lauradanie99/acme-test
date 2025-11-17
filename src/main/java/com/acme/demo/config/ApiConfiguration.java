29854a1c8d5a53f1dd2a2d6291d32c22d22c0264
Laura B <lauradanie99@hotmail.com>
2025-11-16 21:15:30 -0500

    Ajustes de configuración de SSL para llamado a endpoint de mocky.io

--- src/main/java/com/acme/demo/config/ApiConfiguration.java
+++ src/main/java/com/acme/demo/config/ApiConfiguration.java
@@ -1,6 +1,14 @@
 package com.acme.demo.config;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.Collections;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,20 +21,44 @@ import org.springframework.web.client.RestTemplate;
 public class ApiConfiguration {
 
 	@Bean
-	RestTemplate restTemplate() {
-		RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
+    RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyManagementException {
+        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
+        
+        Jaxb2RootElementHttpMessageConverter xmlConverter = new Jaxb2RootElementHttpMessageConverter();
+        restTemplate.setMessageConverters(Collections.singletonList(xmlConverter));
+        
+        return restTemplate;
+    }
 
-		Jaxb2RootElementHttpMessageConverter xmlConverter = new Jaxb2RootElementHttpMessageConverter();
-		restTemplate.setMessageConverters(Collections.singletonList(xmlConverter));
+    @Bean
+    ClientHttpRequestFactory clientHttpRequestFactory() 
+            throws NoSuchAlgorithmException, KeyManagementException {
+        
+        // Deshabilitar validación SSL, se debe quitar cuando el servicio tenga certificados validos
+        TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+        };
 
-		return restTemplate;
-	}
-
-	@Bean
-	ClientHttpRequestFactory clientHttpRequestFactory() {
-		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-		factory.setConnectTimeout(5000);
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+        
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
         factory.setReadTimeout(10000);
-		return factory;
-	}
+        
+        return factory;
+    }
 }
+
